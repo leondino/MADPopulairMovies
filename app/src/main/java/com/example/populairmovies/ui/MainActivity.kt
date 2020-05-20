8a46b62db819ca1764a6c57f08db5d2912983ab6
Léon Imanuel <leondino0309@gmail.com>
2020-05-20 18:50:48 +0200

    Initial commit + Dependecies and permissions

---
+++ app/src/main/java/com/example/populairmovies/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.populairmovies.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.populairmovies.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
